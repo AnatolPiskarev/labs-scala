87b4afd5b3b19c45ef0033a844ec76d70f68ecf4
AnatolPiskarev <tirionlannister1993@gmail.com>
2016-12-20 22:49:27 +0300

    added recursive method for pascal triangle

--- labs/src/Triangle.scala
+++ labs/src/Triangle.scala
@@ -1,17 +1,25 @@
+import scala.annotation.tailrec
+
 /**
   * Created by Anatol on 18.12.16.
   */
 object triangle {
   def main(args: Array[String]): Unit = {
     println(pascal(2, 5)) // case 1 - correct work
-    print(pascal(0,0))  // case 3 - zero element
-    println(pascal(5,2))  // case 2 - element does not exist - throw exception
+    println(pascal(0, 0)) // case 2 - zero element
+    print(pascal(5, 2)) // case 3 - element does not exist - throw exception
   }
+
   def pascal(c: Int, r: Int): Int = {
-    if (c < 0 || r < 0 || c > r) throw new IllegalArgumentException("Column and row numbers must be 0 or greater. Column length must be lower than row length") else{
-      if (c == 0 || c == r) 1 else {
-        pascal(c - 1, r - 1) + pascal(c, r - 1)
-      }
+    @tailrec
+    def pascalTail(c: Int, r: Int, acc: List[Int]): List[Int] = {
+      if (r == 0) acc
+      else pascalTail(c, r - 1,
+        (0 +: acc :+ 0).take(c + 2)
+          .sliding(2, 1).map { x => x.sum }.toList)
     }
-  }
+    if (c == 0 || c == r) 1
+    else if (c < 0 || r < 0 || c > r) throw new IllegalArgumentException("Column and row numbers must be 0 or greater. Column length must be lower than row length")
+      else pascalTail(c, r, List(1))(c)
+    }
 }
